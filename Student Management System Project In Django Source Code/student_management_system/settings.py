96688e0c9e79975926f15ffe7231ad2a44b6405a
Tran Quang Linh <linhdautu@gmail.com>
2021-11-24 22:23:02 +0700

    second commit

--- Student Management System Project In Django Source Code/student_management_system/settings.py
+++ Student Management System Project In Django Source Code/student_management_system/settings.py
@@ -1,7 +1,9 @@
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf.global_settings import DATABASES
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -68,20 +70,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
-
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sv7',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3309',
-    }
-}
+#DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'sv7',
+#        'USER': 'root',
+#        'PASSWORD': '123456',
+#        'HOST': 'localhost',
+#       'PORT': '3309',
+#    }
+#}
 
 
 # Password validation
